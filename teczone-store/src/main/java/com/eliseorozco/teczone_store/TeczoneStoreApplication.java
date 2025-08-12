2f171dffe74156f4792d7104fc21f302a1c20c55
Eliseo Orozco <eliseorozcow@gmail.com>
2025-08-11 22:06:56 -0300

    Setup Project

---
+++ teczone-store/src/main/java/com/eliseorozco/teczone_store/TeczoneStoreApplication.java
@@ -0,0 +1,13 @@
+package com.eliseorozco.teczone_store;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TeczoneStoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TeczoneStoreApplication.class, args);
+	}
+
+}
---
+++ teczone-store/src/test/java/com/eliseorozco/teczone_store/TeczoneStoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eliseorozco.teczone_store;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TeczoneStoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
